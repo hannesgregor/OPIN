996d15ff625637701033978803bea8ff820720e1
hannes <hannesgregor@HannesGegorsMBP.lan>
2021-05-13 18:44:18 +0300

    [Delivers #178144277] Show Loading and Error States

--- src/store/__tests__/restaurants.spec.js
+++ src/store/__tests__/restaurants.spec.js
@@ -1,33 +1,132 @@
-import {createStore, applyMiddleware} from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import restaurantsReducer from '../restaurants/reducers';
-import {loadRestaurants} from '../restaurants/actions';
+import { loadRestaurants } from '../restaurants/actions';
 
 describe('restaurants', () => {
   describe('loadRestaurants action', () => {
-    it('stores the restaurants', async () => {
+    describe('when loading succeeds', () => {
       const records = [
-        {id: 1, name: 'Sushi Place'},
-        {id: 2, name: 'Pizza Place'},
+        { id: 1, name: 'Sushi Place' },
+        { id: 2, name: 'Pizza Place' },
       ];
 
-      const api = {
-        loadRestaurants: () => Promise.resolve(records),
-      };
+      let store;
 
-      const initialState = {
-        records: [],
-      };
+      beforeEach(() => {
+        const api = {
+          loadRestaurants: () => Promise.resolve(records),
+        };
 
-      const store = createStore(
+        const initialState = {
+          records: [],
+        };
+
+        store = createStore(
+          restaurantsReducer,
+          initialState,
+          applyMiddleware(thunk.withExtraArgument(api)),
+        );
+
+        return store.dispatch(loadRestaurants());
+      });
+
+      it('stores the restaurants', () => {
+
+        expect(store.getState().records).toEqual(records);
+      });
+
+      it('clears the loading flag', () => {
+        expect(store.getState().loading).toEqual(false);
+      });
+    });
+
+    describe('when loading fails', () => {
+      let store;
+
+      beforeEach(() => {
+        const api = {
+          loadRestaurants: () => Promise.reject(),
+        };
+
+        const initialState = {};
+
+        store = createStore(
+          restaurantsReducer,
+          initialState,
+          applyMiddleware(thunk.withExtraArgument(api)),
+        );
+
+        return store.dispatch(loadRestaurants());
+      });
+
+      it('sets an error flag', () => {
+        expect(store.getState().loadError).toEqual(true);
+      });
+
+      it('clears the loading flag', () => {
+        expect(store.getState().loading).toEqual(false);
+      });
+    });
+
+    describe('when loading fails', () => {
+      beforeEach(() => {
+        renderWithProps({ loadError: true });
+      });
+
+      it('displays the error message', () => {
+        const { queryByText } = context;
+        expect(queryByText('Restaurants could not be loaded.')).not.toBeNull();
+      });
+    });
+
+    describe('while loading', () => {
+      let store;
+
+      beforeEach(() => {
+        const api = {
+          loadRestaurants: () => new Promise(() => { }),
+        };
+
+        const initialState = { loadError: true };
+
+        store = createStore(
+          restaurantsReducer,
+          initialState,
+          applyMiddleware(thunk.withExtraArgument(api)),
+        );
+
+        store.dispatch(loadRestaurants());
+      });
+      it('sets a loading flag', () => {
+        expect(store.getState().loading).toEqual(true);
+      });
+
+      it('clears the error flag', () => {
+        expect(store.getState().loadError).toEqual(false);
+      });
+    });
+
+  });
+
+  describe('initially', () => {
+    let store;
+
+    beforeEach(() => {
+      const initialState = {};
+
+      store = createStore(
         restaurantsReducer,
         initialState,
-        applyMiddleware(thunk.withExtraArgument(api)),
+        applyMiddleware(thunk),
       );
+    });
+    it('does not have the loading flag set', () => {
+      expect(store.getState().loading).toEqual(false);
+    });
 
-      await store.dispatch(loadRestaurants());
-
-      expect(store.getState().records).toEqual(records);
+    it('does not have the error flag set', () => {
+      expect(store.getState().loadError).toEqual(false);
     });
   });
 });
