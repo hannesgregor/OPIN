f78e737f7114f4b725f37be97d55b74f6c07521f
hannes <hannesgregor@HannesGegorsMBP.lan>
2021-05-13 16:15:20 +0300

    [Delivers #178143804] Added list restaurants

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,24 +1,29 @@
-import {useEffect} from 'react';
-import {connect} from 'react-redux';
-import {loadRestaurants} from '../store/restaurants/actions';
+import { useEffect } from 'react';
+import { connect } from 'react-redux';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import { loadRestaurants } from '../store/restaurants/actions';
 
-export const RestaurantList = ({loadRestaurants, restaurants}) => {
-  useEffect(() => {
-    loadRestaurants();
-  }, [loadRestaurants]);
-  return (
-    <ul>
-      {restaurants.map(restaurant => (
-        <li key={restaurant.id}>{restaurant.name}</li>
-      ))}
-    </ul>
-  );
+export const RestaurantList = ({ loadRestaurants, restaurants }) => {
+    useEffect(() => {
+        loadRestaurants();
+    }, [loadRestaurants]);
+    return (
+        <List>
+            {restaurants.map(restaurant => (
+                <ListItem key={restaurant.id}>
+                    <ListItemText>{restaurant.name}</ListItemText>
+                </ListItem>
+            ))}
+        </List>
+    );
 };
 
 const mapStateToProps = state => ({
-  restaurants: state.restaurants.records,
+    restaurants: state.restaurants.records,
 });
 
-const mapDispatchToProps = {loadRestaurants};
+const mapDispatchToProps = { loadRestaurants };
 
 export default connect(mapStateToProps, mapDispatchToProps)(RestaurantList);
