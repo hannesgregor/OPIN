9ec60722da62cb850b3f5cbe150c8f24e769933e
hannes <hannesgregor@HannesGegorsMBP.lan>
2021-05-13 14:42:53 +0300

    Add RestaurantScreen and RestaurantList

---
+++ src/components/RestaurantScreen.js
@@ -0,0 +1,11 @@
+import RestaurantList from './RestaurantList';
+
+
+const RestaurantScreen = () => (
+    <div>
+        <h1>Restaurants</h1>
+        <RestaurantList />
+    </div>
+);
+
+export default RestaurantScreen;
